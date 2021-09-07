29fbf49d565970903686a5bed86205e9d3f30cde
ralexrivero <ralexrivero@gmail.com>
2021-09-07 14:14:59 -0400

    change conditional

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    last = (number * -1) % 10
+    last = (number % -10)
 else:
     last = (number % 10)
 if last > 5:
@@ -11,6 +11,6 @@ if last > 5:
 elif last < 6 and last != 0:
     print("Last digit of {} is {} and is less than 6 and not 0"
           .format(number, last))
-else:
+elif last == 0:
     print("Last digit of {} is {} and is 0"
           .format(number, last))
