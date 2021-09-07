c04df9bdf73e4c64b031c0f025eff1f31e688376
ralexrivero <ralexrivero@gmail.com>
2021-09-07 20:35:39 +0000

    combination 3

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{0:d}{1:d}".format(i, j)
+        else:
+            print("{0:d}{1:d}".format(i, j), end=", ")
