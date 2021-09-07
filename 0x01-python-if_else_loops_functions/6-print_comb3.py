bd8d9de92c5c6419a80346600e7d5a167070ba30
ralexrivero <ralexrivero@gmail.com>
2021-09-07 19:31:56 -0400

    fix braces

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 10):
     for j in range(i + 1, 10):
         if i == 8 and j == 9:
-            print("{0:d}{1:d}".format(i, j)
+            print("{0:d}{1:d}".format(i, j))
         else:
             print("{0:d}{1:d}".format(i, j), end=", ")
