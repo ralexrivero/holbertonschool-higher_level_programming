39b6a00732ebe5c908e25b3021fbf5e8963c81f1
ralexrivero <ralexrivero@gmail.com>
2021-09-09 22:01:52 -0400

    mul args

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    i = 0
+    sum = 0
+    for i in range(len(sys.argv) - 1):
+        sum += int(sys.argv[i + 1])
+    print('{:d}'.format(sum))
