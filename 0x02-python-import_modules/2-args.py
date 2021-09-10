eb87d36042e7529e71ca5703832cc11825eab81d
ralexrivero <ralexrivero@gmail.com>
2021-09-09 21:52:16 -0400

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
-                
+if __name__ == "__main__":
+    import sys
+    size = (len(sys.argv) - 1)
+    if size == 0:
+        print('0 arguments.')
+    elif size == 1:
+        print('{:d} argument:'.format(size))
+        print('{:d}: {:s}'.format(size, sys.argv[size]))
+    else:
+        index = 1
+        print('{:d} arguments:'.format(size))
+        while index < (len(sys.argv)):
+            print('{:d}: {:s}'.format(index, sys.argv[index]))
+            index += 1
