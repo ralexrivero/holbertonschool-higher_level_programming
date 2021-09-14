4bf941b9744ca59651b21f3e9a2c4aeb098d6c16
ralexrivero <ralexrivero@gmail.com>
2021-09-14 02:24:14 +0000

    print matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for index, i in enumerate(row):
+            print("{:d}".format(i), end="")
+            if (index < len(row) - 1):
+                print("{}".format(" "), end="")
+         print()
