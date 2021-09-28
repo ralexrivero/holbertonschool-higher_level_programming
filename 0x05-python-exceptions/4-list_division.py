8c506688b28947657e5f34653febb349ae771b22
ralexrivero <ralexrivero@gmail.com>
2021-09-27 20:59:25 -0400

    fix indent

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,13 +4,13 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length):
         try:
             new_list.append(my_list_1[i] / my_list_2[i])
+        except (ValueError, TypeError):
+            print("wrong type")
         except ZeroDivisionError:
             print("division by 0")
-        except TypeError:
-            print("wrong type")
         except IndexError:
             print("out of range")
         finally:
             pass
         new_list.append(0)
-        return new_list
+    return new_list
