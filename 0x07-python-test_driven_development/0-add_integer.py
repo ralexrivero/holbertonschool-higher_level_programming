511772a629e2d8b16c05aa5606179b68f67191be
ralexrivero <ralexrivero@gmail.com>
2021-10-05 20:06:58 -0300

    raise error

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
+""" adds 2 integers """
+
 
 def add_integer(a, b=98):
-    a_i = int(a)
-    b_i = int(b)
-    res = a_i + b_i
+    """
+        adds two integers
+        Args:
+            a (int): first value
+            b (int, optional): second value
+        Returns:
+            adition of a and b
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    else:
+        a = int(a)
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        b = int(b)
+    res = a + b
     return res
