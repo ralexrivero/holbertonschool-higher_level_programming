e9bbed81864998f9c9aa1eac025ed847a2a24343
ralexrivero <ralexrivero@gmail.com>
2021-11-16 18:16:12 -0800

    full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,10 @@
 -- create a table in a given database
 -- database passed as argument of command mysql
-
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256),`score` INT);
+-- add new rows into the created if not exist table
+INSERT INTO `second_table` (`id`, `name`, `score`)
+VALUES
+	(1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George', 8);
