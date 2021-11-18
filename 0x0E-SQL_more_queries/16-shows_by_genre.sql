c80c921afb0512966bd7083424e6b26df9502981
ralexrivero <ralexrivero@gmail.com>
2021-11-17 18:10:07 -0800

    fix syntax

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,5 @@
 -- Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 15-comedy_only.sql)
-        tv_genres.name
-FROM tv_show_genres
-        RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-        LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-ORDER BY tv_shows.title,
-        tv_genres.name ASC;
-        
+SELECT tv_shows.title AS 'title', tv_genres.name AS 'name'
+FROM tv_shows LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT OUTER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY title, name;        
