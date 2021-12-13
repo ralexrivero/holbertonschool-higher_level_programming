501243d140c08335bcec8a8c9361e995d61992b4
ralexrivero <ralexrivero@gmail.com>
2021-12-13 14:34:57 -0300

    repeat

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const entry = process.argv;
+const side = parseInt(entry[2]);
+let x;
+
+if (isNaN(side)) {
+  console.log('Missing size');
+} else {
+  for (x = 0; x < side; x++) {
+    console.log('x'.repeat(side));
+  }
+}
