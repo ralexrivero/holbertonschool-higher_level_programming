6621be69d425f4066fe0ecbf32c7ace432145725
ralexrivero <ralexrivero@gmail.com>
2021-12-13 15:16:32 -0300

    sort and length

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const entry = process.argv;
+let nearBig = entry.sort();
+nearBig = entry[entry.length - 2];
+
+if (entry.length < 3) {
+  console.log(0);
+} else {
+  console.log(nearBig);
+}
