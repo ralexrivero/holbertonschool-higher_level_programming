67ea6acb702c7216cf6c9b654d79161ad330019e
ralexrivero <ralexrivero@gmail.com>
2021-12-13 15:20:26 -0300

    arguments conditional fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ const entry = process.argv;
 let nearBig = entry.sort();
 nearBig = entry[entry.length - 2];
 
-if (entry.length < 3) {
+if (entry.length <= 3) {
   console.log(0);
 } else {
   console.log(nearBig);
