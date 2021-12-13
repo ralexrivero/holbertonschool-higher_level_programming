1e260817939a5acc26bc098793c0a9ef11e10fef
ralexrivero <ralexrivero@gmail.com>
2021-12-13 16:59:53 -0300

    order numbers in list

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,19 @@
 #!/usr/bin/node
 
 const entry = process.argv;
-let nearBig = entry.sort();
-nearBig = entry[entry.length - 2];
-
+const toOrder = [];
 if (entry.length <= 3) {
   console.log(0);
 } else {
-  console.log(nearBig);
+  for (let i = 2; i < entry.length; i++) {
+    const check = parseInt(entry[i]);
+    if (isNaN(check)) {
+      continue;
+    } else {
+      toOrder.push(check);
+    }
+  }
 }
+const x = toOrder.length;
+toOrder.sort(function (a, b) { return a - b; });
+console.log(toOrder[x - 2]);
