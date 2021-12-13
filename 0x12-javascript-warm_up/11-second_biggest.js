26d51090aab7ccd67380ce4ae80edec82d1843ba
ralexrivero <ralexrivero@gmail.com>
2021-12-13 17:08:55 -0300

    reorder variables

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,19 +2,24 @@
 
 const entry = process.argv;
 const toOrder = [];
+let check;
+
+function secBig() {
+  const x = toOrder.length;
+  toOrder.sort(function (a, b) { return a - b; });
+  console.log(toOrder[x - 2]);
+}
+
 if (entry.length <= 3) {
   console.log(0);
 } else {
   for (let i = 2; i < entry.length; i++) {
-    const check = parseInt(entry[i]);
+    check = parseInt(entry[i]);
     if (isNaN(check)) {
       continue;
     } else {
       toOrder.push(check);
-      const x = toOrder.length;
-      toOrder.sort(function (a, b) { return a - b; });
-      console.log(toOrder[x - 2]);
     }
   }
+  secBig();
 }
-
