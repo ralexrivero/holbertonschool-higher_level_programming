4b031e2b1df5135a47f76020b862de4c656c5b2f
ralexrivero <ralexrivero@gmail.com>
2021-12-14 23:47:39 -0300

    advanced

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+
+const { list } = require('./100-data');
+const newList = list.map((value, index) => {
+  return value * index;
+});
+console.log(list);
+console.log(newList);
--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const dict = require('./101-data').dict;
+const newDict = {};
+
+for (const [key, value] of Object.entries(dict)) {
+  if (newDict[value] === undefined) {
+    newDict[value] = [key];
+  } else {
+    newDict[value].push(key);
+  }
+}
+
+console.log(newDict);
