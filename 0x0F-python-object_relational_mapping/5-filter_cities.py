beb8b19e34f0e5c1b39200496b364329d7f63a14
ralexrivero <ralexrivero@gmail.com>
2021-12-18 18:22:24 -0300

    order ambiguous fix

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
         WHERE
             states.name = %s
         ORDER BY
-            id ASC;
+            cities.id ASC;
     """
 
     cur.execute(sql, (state,))
