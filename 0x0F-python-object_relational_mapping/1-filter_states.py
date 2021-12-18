741fc5f2c7787050bc66401b2031cbf017ae5da9
ralexrivero <ralexrivero@gmail.com>
2021-12-18 02:40:08 -0300

    simplify argv

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,17 +4,12 @@ List all states with a name starting with N (upper N)
 """
 
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
     """ not be executed when imported """
-    host = 'localhost'
-    port = 3306
-    username = sys.argv[1]
-    password = sys.argv[2]
-    name = sys.argv[3]
-    conn = MySQLdb.connect(host=host, port=port, user=username,
-                           passwd=password, db=name, charset="utf8")
+    conn = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' \
 ORDER BY states.id ASC")
