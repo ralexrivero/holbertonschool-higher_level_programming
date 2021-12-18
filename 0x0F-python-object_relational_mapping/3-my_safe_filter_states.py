7ca343c33413fb24026e046eb306c13362c00149
ralexrivero <ralexrivero@gmail.com>
2021-12-18 14:52:54 -0300

    slq injection free

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,25 +1,40 @@
 #!/usr/bin/python3
 """
-takes in arguments and sisplays all values in the sates table of database
-where name matches the argument, safe from MySQL injections
+Takes in an argument and displays all values in the states tables of a database
+where name matches the argument
 """
 
 import MySQLdb
 import sys
 
-host = 'localhost'
-port = 3306
-username = sys.argv[1]
-password = sys.argv[2]
-name = sys.argv[3]
-
-
 if __name__ == "__main__":
     """ not be executed when imported """
-    conn = MySQLdb.connect(host=host, port=port, user=username,
-                           passwd=password, db=name, charset="utf8")
+    host = 'localhost'
+    port = 3306
+    username = sys.argv[1]
+    password = sys.argv[2]
+    name = sys.argv[3]
+    state = str(sys.argv[4])
+
+    conn = MySQLdb.connect(host=host,
+                           port=port,
+                           user=username,
+                           passwd=password,
+                           db=name,
+                           charset="utf8"
+                           )
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name like 'N%' ORDER BY id ASC")
+    sql = """
+        SELECT
+            *
+        FROM
+            states
+        WHERE
+            name LIKE BINARY %(state)
+          ORDER BY id ASC""", {
+              'state': state
+          }
+    cur.execute(sql)
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
