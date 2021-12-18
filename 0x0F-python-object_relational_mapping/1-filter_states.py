9557c56dd43d7c5d5a3dc1975b7944ae06b8c557
ralexrivero <ralexrivero@gmail.com>
2021-12-18 02:55:50 -0300

    add triple quotes

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
                            db=argv[3],
                            charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' \
-ORDER BY states.id ASC")
+    cur.execute("""SELECT * FROM states WHERE name LIKE 'N%'
+                ORDER BY states.id ASC""")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
