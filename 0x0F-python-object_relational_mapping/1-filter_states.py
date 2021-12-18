63e421fc603abfc9867d0a57dade864d1ba361c6
ralexrivero <ralexrivero@gmail.com>
2021-12-18 03:07:40 -0300

    add binary

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,10 +3,10 @@
 List all states with a name starting with N (upper N)
 """
 
-import MySQLdb
-from sys import argv
 if __name__ == "__main__":
     """ not be executed when imported """
+    import MySQLdb
+    from sys import argv
     conn = MySQLdb.connect(host='localhost',
                            port=3306,
                            user=argv[1],
@@ -14,7 +14,7 @@ if __name__ == "__main__":
                            db=argv[3],
                            charset="utf8")
     cur = conn.cursor()
-    cur.execute("""SELECT * FROM states WHERE name LIKE 'N%'
+    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%'
                 ORDER BY states.id ASC""")
     query_rows = cur.fetchall()
     for row in query_rows:
