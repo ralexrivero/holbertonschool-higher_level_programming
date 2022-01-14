62a971eb23d3e445c2cd312e6ff21a9acfcf6abe
ralexrivero <ralexrivero@gmail.com>
2022-01-14 12:36:16 -0500

    fix syntax error

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL (uniform resource locator) as an argument, send post request, display the body of the response
-curl -sX POST -d 'email=test@gmail.com' -d 'subject=I will allways be here for PLD' "S1"
+# takes in a URL (uniform resource locator) as an argument, send post request, displa4y the body of the response
+curl -sX POST -d 'email=test@gmail.com' -d 'subject=I will allways be here for PLD' "$1"
