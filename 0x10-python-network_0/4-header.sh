ff758648397c7e1b23cfeb9bd587f02c2f61b04e
ralexrivero <ralexrivero@gmail.com>
2022-01-14 02:12:20 -0300

    fix header value pair

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # takes a URL (uniform resource locator) as an argument, sends a GET request, and displays the body of the response
 # header variable X-School-User-Id must be sent with the value 98
-curl -s -H "X-HolbertonSchool-User-Id:98" -X GET "$1"
+curl -s -H "X-School-User-Id:98" -X GET "$1"
