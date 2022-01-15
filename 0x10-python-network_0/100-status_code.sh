8a97230041e41424c8b493ffcd414cccf9424d2d
ralexrivero <ralexrivero@gmail.com>
2022-01-14 23:25:27 -0300

    base files

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-# 
+# sends a request to a URL passed as an argument, and displays only the status code of the response.
---
+++ 0x10-python-network_0/101-post_json.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+# sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
