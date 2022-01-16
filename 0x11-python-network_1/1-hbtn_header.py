b02275fcddaa5431066f631da098290cd430410c
ralexrivero <ralexrivero@gmail.com>
2022-01-16 14:58:25 -0300

    docstring start after """

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-Takes in a URL, sends a request to the URL and displays
+""" Takes in a URL, sends a request to the URL and displays
 the value of the X-Request-Id variable found in the header of the response
 """
 
