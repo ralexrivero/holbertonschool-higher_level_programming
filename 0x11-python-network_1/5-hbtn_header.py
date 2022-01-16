1a1f298e9e085d4e927a34834d225f71f344df39
ralexrivero <ralexrivero@gmail.com>
2022-01-16 18:50:01 -0300

    requests.headers

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Takes in a URL, sends a request to the URL
+    Displays the value of the variable X-Request-Id in the response header
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    r = requests.get(url)
+    print(r.headers['X-Request-Id'])
