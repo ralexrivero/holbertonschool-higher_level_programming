f84f3c8a9d31347ce287ba6b89f40007346327de
ralexrivero <ralexrivero@gmail.com>
2022-01-16 16:42:42 -0300

    error handler

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    Takes in a URL, sends a request to the URL
+    and displays the body of the response (decoded in utf-8).
+    manage urllib.error.HTTPError
+        exceptions and print:
+            Error code: followed by the HTTP status code
+"""
+
+from sys import argv
+from urllib import request, error
+
+url = argv[1]
+
+
+def req(url):
+    try:
+        with request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
+
+
+if __name__ == "__main__":
+    req(url)
